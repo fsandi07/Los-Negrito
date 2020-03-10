7da4a690506c072dded07efa88de5a8c565f3aed
Carlos Lezcano <carlos.lezcano988@gmail.com>
2020-03-09 18:03:13 -0600

    modulo agregar usuarios

---
+++ SIGAPRO/SIGAPRO/Vistas/Agregar_usuarios.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SIGAPRO.NEGOCIO;
+
+namespace SIGAPRO.Vistas
+{
+    public partial class Agregar_usuarios : System.Web.UI.Page
+    {
+        private Usuarios usu;
+        private UsuariosHelper usuariosHelper;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_agregar_usuario_Click(object sender, EventArgs e)
+        {
+            if (this.txt_clave1.Text==this.txt_clave2.Text)
+            {
+
+                this.usu = new Usuarios();
+                this.usu.Cedula_usuario = this.txt_cedula.Text;
+                this.usu.Nombre_usuario = this.txt_nombre.Text;
+                this.usu.Apellido1 = this.txt_apellido1.Text;
+                this.usu.Apellido2 = this.txt_apellido2.Text;
+                this.usu.Nick_name = this.txt_nickname.Text;
+                this.usu.Correo_electronico = this.txt_correo.Text;
+                this.usu.Clave_usuario = this.txt_clave2.Text;
+                this.usu.Rol = "administrador";
+                this.usu.Estado = 1;
+                this.usu.Opc = 1;
+                this.usuariosHelper = new UsuariosHelper(usu);
+                this.usuariosHelper.Agregar_Usuarios();
+                ScriptManager.RegisterStartupScript(this, typeof(Page), "mensajeDeconfirmacion", "mensajeDeconfirmacion('" + "" + "');", true);
+
+            }
+            else
+            {
+                this.txt_clave2.Text = "las contraseñas no coinciden por favor verifique";
+
+
+            }
+            
+        }
+    }
+}
---
+++ SIGAPRO/SIGAPRO/Vistas/Agregar_usuarios.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SIGAPRO.Vistas {
+    
+    
+    public partial class Agregar_usuarios {
+        
+        /// <summary>
+        /// Control txt_cedula.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_cedula;
+        
+        /// <summary>
+        /// Control txt_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nombre;
+        
+        /// <summary>
+        /// Control txt_apellido1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_apellido1;
+        
+        /// <summary>
+        /// Control txt_apellido2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_apellido2;
+        
+        /// <summary>
+        /// Control txt_nickname.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nickname;
+        
+        /// <summary>
+        /// Control txt_correo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_correo;
+        
+        /// <summary>
+        /// Control txt_clave1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_clave1;
+        
+        /// <summary>
+        /// Control txt_clave2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_clave2;
+        
+        /// <summary>
+        /// Control btn_agregar_usuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_agregar_usuario;
+    }
+}
